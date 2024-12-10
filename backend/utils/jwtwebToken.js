1fe99b181f8d882b43b9e2349bbb313e7af1959e
tosif66 <motosifansari66@gmail.com>
2024-12-10 15:54:33 +0530

    #

--- backend/utils/jwtwebToken.js
+++ backend/utils/jwtwebToken.js
@@ -8,7 +8,7 @@ const jwtToken = (userId,res) =>{
         maxAge: 30*24*60*1000,
         httpOnly:true,
         sameSite:"strict",
-        secure:process.env.SECURE !== 'development'
+        secure:true
     })
 }
 
