776a871f1ab0d7c4deb9dabad1c9a356b503aca0
tosif66 <motosifansari66@gmail.com>
2024-12-13 12:10:46 +0530

    #

--- frontend/src/home/Home.jsx
+++ frontend/src/home/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import MessageContainer from "./components/MessageContainer";
 import Sidebar from "./components/Sidebar";
-import "./mycss/Home.css";
+import "../mycss/Home.css";
 
 const Home = () => {
   const [selectedUser, setSelectedUser] = useState(null);
